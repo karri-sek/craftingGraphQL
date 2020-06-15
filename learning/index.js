9c3ef35187bc15ce88f28ac4356aa95453bc0212
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2020-06-15 18:27:15 +0100

    Adding graphql to the project done

--- learning/index.js
+++ learning/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
 const app = express();
 app.get('/', (req, res) => {
     res.send("Graph QL is amazing")
 });
+
+
+const root = { hello: () => "Hi, I'm sekhar"}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(2020, () => console.log(" runnning server at http://localhost:2020/graphql"))
